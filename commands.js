d14eb2c0ba51ed2b2bac1e4988e92b2276b4c527
Maxime Moreillon <m.moreillon@gmail.com>
2020-02-27 01:01:19 +0000

    debugging commands error

--- commands.js
+++ commands.js
@@ -12,6 +12,7 @@ module.exports = [
       utils.send_response(reply_token,"Hello to you too!");
     }
   },
+
   {
     message: "balance",
     private: true,
@@ -36,10 +37,12 @@ module.exports = [
 
     }
   },
+  
   {
     message: "weight",
     private: true,
     command: (reply_token) => {
+
       jwt.sign({ service_name: 'line_bot' }, secrets.jwt_secret, (err, token) => {
         if(err) return utils.send_response(reply_token, "Error generating JWT")
         axios.post(secrets.weight_api_url, {},{
